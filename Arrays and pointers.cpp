bf83c881642c13ddf6c5a59598fab6ea05724f87
Harshal <harshalchaudhary03@gmail.com>
2022-05-18 15:07:42 +0530

    Added args but no return code and modified the arrays and pointers

--- Arrays and pointers.cpp
+++ Arrays and pointers.cpp
@@ -1,13 +1,10 @@
-//Array and pointers
 #include<iostream>
 using namespace std;
-
-int main(){
- int a[3]={10,20,30};
- int *ptr=&a[0];
- for(int i=0;i<3;i++)
- {
- 	cout<<*ptr<<" ";
- 	ptr++;
-	 }	
+int main()
+{
+	int a[3]={10,20,30};
+	for(int i=0;i<3;i++)
+	{
+		cout<<a[i]<<" ";
+	}
 }
---
+++ args but no return.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int fn(int);
+int main()
+{
+	int a=10,z;
+	z=fn(a);
+	cout<<z;
+	return 0;
+}
+int fn(int a)
+{
+	int p;
+	p=x*2;
+	return(p);
+}
+
